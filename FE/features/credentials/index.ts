3fb7a268c696aa90426ee9034cf11895130592db
takieeldeen <takie.eldeen1998@gmail.com>
2026-03-07 14:50:46 +0200

    Fixing Build Issue

--- FE/features/credentials/index.ts
+++ FE/features/credentials/index.ts
@@ -1,6 +1,6 @@
-export * from "./components/credentials-list";
+export * from "./components/credentials-list-view";
 export * from "./components/credential-item";
 export * from "./components/credential-empty";
 export * from "./components/credential-loading";
 export * from "./components/credential-error";
-export * from "./components/credential-not-found";
+// export * from "./components/";
